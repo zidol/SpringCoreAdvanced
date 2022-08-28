ec595fd8d06c40675f6dbf247e038d4bc34c6c3a
zidol <ajh3166@naver.com>
2022-08-28 22:58:21 +0900

    로그 추적기 V2 - 파라미터로 동기화

---
+++ src/test/java/hello/advanced/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,28 @@
+package hello.advanced.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(),"hello2");
+        trace.exception(status2, new IllegalArgumentException());
+        trace.exception(status1, new IllegalArgumentException());
+    }
+}
