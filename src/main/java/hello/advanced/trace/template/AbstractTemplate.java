c6a81010d71806ae7b1357aedee62497d1a997aa
zidol <ajh3166@naver.com>
2022-09-03 17:35:43 +0900

    전략 패턴 예제2

--- src/main/java/hello/advanced/trace/template/AbstractTemplate.java
+++ src/main/java/hello/advanced/trace/template/AbstractTemplate.java
@@ -3,6 +3,12 @@ package hello.advanced.trace.template;
 import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.logtrace.LogTrace;
 
+/**
+ * 템플릿 메서드 패턴 V1
+ * 핵심 기능 부분은 변하고, 로그 추적기를 사용하는 부분은 변하지 않는 부분
+ *  이 둘을 분리해서 모듈화 함
+ * @param <T>
+ */
 public abstract class AbstractTemplate<T> {
 
     private final LogTrace trace;
